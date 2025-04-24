31612d6deed5f979bb049fdb540d92ca38ddc652
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-04-24 08:02:49 +0200

    start refactoring

---
+++ projects/ngx-notifications/src/lib/ngx-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-notifications',
+  imports: [],
+  template: `
+    <p>
+      ngx-notifications works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxNotificationsComponent {
+
+}
---
+++ projects/ngx-notifications/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-notifications
+ */
+
+export * from './lib/ngx-notifications.service';
+export * from './lib/ngx-notifications.component';
